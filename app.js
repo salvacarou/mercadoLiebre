82d3aed5d680009972f21780f6c085f006651463
salvacarou <salvadorcarou@gmail.com>
2021-08-17 21:54:10 -0300

    Guardando MercadoLiebre

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+
+app.listen(3000, () => {
+    console.log("funciona");
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/home.html"));
+});
+
+app.get("/home", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/home.html"))
+})
+
+app.get("/ingresar", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/enter.html"))
+})
+
+app.get("/registrarse", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/register.html"))
+})
